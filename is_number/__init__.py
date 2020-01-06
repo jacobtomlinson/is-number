f33340fd882fa21166a8a55b535e77de1e084578
Jacob Tomlinson <jtomlinson@nvidia.com>
2020-01-06 16:14:38 +0000

    Add versioneer

--- is_number/__init__.py
+++ is_number/__init__.py
@@ -1,6 +1,12 @@
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+
 def is_number(in_value):
     try:
         float(in_value)
         return True
     except ValueError:
         return False
+
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 import setuptools
+import versioneer
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
@@ -7,7 +8,8 @@ with open("requirements.txt", "r") as fh:
 
 setuptools.setup(
     name="is-number",
-    version="0.0.1",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     author="Jacob Tomlinson",
     author_email="jacob@tomlinson.email",
     description="A Python library to determine if something is a number.",
